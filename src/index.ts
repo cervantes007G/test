bdc49ecddfc5c823ff3eb4e9c4f9db5202b807c3
Alejandro Martell Guevara <alejandro.mguevara@mcsystems.com>
2022-10-06 09:33:26 -0400

    fix: blank space

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export const sum = (number1: number, number2: number) => number1 + number2;
 export const rest = (number1: number, number2: number) => number1 - number2;
 export const multiply = (number1: number, number2: number) => number1 * number2;
+
 export const divide = (number1: number, number2: number) => number1 / number2;
